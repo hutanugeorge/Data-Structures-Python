8ed03f5b078c8785aa7be979a817af6c20918b72
hutanugeorge <74975326+hutanugeorge@users.noreply.github.com>
2021-01-03 02:52:50 +0200

    Graphs implementation

---
+++ Graphs.py
@@ -0,0 +1,66 @@
+class Graph:
+    def __init__(self):
+        self._number_of_nodes = 0
+        self._adjacent_list = dict()
+
+    def add_vertex(self, node):
+        if node in self._adjacent_list:
+            raise ValueError('You can\'t add 2 nodes wioth the same value!')
+        self._adjacent_list[node] = []
+        self._number_of_nodes += 1
+        self._edge_list = dict()
+        self._count_edges = 0
+        return self
+
+    def add_edge(self, node1, node2, directed = False, weighted = False):
+
+        if (node1 or node2) not in self._adjacent_list:
+            raise ValueError('This node isn\'t in  the graph!')
+        elif node2 in self._adjacent_list[node1]:
+            raise ValueError('This edge already exist!')
+        elif node1 in self._adjacent_list[node2]:
+            raise ValueError('This edge already exist!')
+
+        if not directed:
+            if not isinstance(weighted, int):
+                self._adjacent_list[node1].append(node2)
+                self._adjacent_list[node2].append(node1)
+            else:
+                self._adjacent_list[node1].append(node2)
+                self._adjacent_list[node2].append(node1)
+                self._edge_list[self._count_edges] = [node1, node2, weighted]
+                self._count_edges += 1
+        else:
+            if not weighted:
+                self._adjacent_list[node1].append(node2)
+            else:
+                self._adjacent_list[node1].append(node2)
+                self._edge_list[self._count_edges] = [node1, node2, weighted]
+                self._count_edges += 1
+
+        return self
+
+    def print(self):
+        for i in self._adjacent_list:
+            print(i, '-->', end = ' ')
+            print([x for x in self._adjacent_list[i]])
+        for j in self._edge_list:
+            print(j, '-->', end = ' ')
+            print([x for x in self._edge_list[j]])
+
+
+my_graph = Graph()
+my_graph.add_vertex(0)
+my_graph.add_vertex(1)
+my_graph.add_vertex(2)
+my_graph.add_vertex(3)
+my_graph.add_vertex(4)
+my_graph.add_edge(0, 1, directed = True, weighted = 99)
+my_graph.add_edge(0, 2, directed = True, weighted = 50)
+my_graph.add_edge(1, 2, directed = True, weighted = 50)
+my_graph.add_edge(1, 3, directed = True, weighted = 50)
+my_graph.add_edge(1, 4, directed = True, weighted = 50)
+my_graph.add_edge(2, 3, directed = True, weighted = 99)
+my_graph.add_edge(3, 4, directed = True, weighted = 75)
+
+my_graph.print()
